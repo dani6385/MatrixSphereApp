addff991f2b3a90eb04e885429bc39f9221f9a3b
dani6385 <dani6385@gmail.com>
2026-06-16 07:51:05 +0000

    login mikrotik

--- apps/client_hotspot/lib/bottom/scan_bottom.dart
+++ apps/client_hotspot/lib/bottom/scan_bottom.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../dialog/scan_dialog.dart';
+import 'scan_dialog.dart';
 
 class ScanBottom extends StatefulWidget {
   const ScanBottom({super.key});
