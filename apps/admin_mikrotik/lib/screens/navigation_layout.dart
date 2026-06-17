9af468338e7a9f1a05060a869a34db2e9b925406
dani6385 <dani6385@gmail.com>
2026-06-18 00:23:55 +0700

    data baru

---
+++ apps/admin_mikrotik/lib/screens/navigation_layout.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:shared_ui/shared_ui.dart';
+import '../constants/pages_config.dart';
+
+class NavigationLayout extends StatefulWidget {
+  const NavigationLayout({super.key});
+
+  @override
+  State<NavigationLayout> createState() => _NavigationLayoutState();
+}
+
+class _NavigationLayoutState extends State<NavigationLayout> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PagesConfig.pages[_selectedIndex],
+      bottomNavigationBar: AppBottomNavBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.wifi), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.group), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: ""),
+        ],
+      ),
+    );
+  }
+}
--- apps/client_hotspot/lib/services/hotspot_service.dart
+++ apps/client_hotspot/lib/services/hotspot_service.dart
@@ -11,7 +11,18 @@ class HotspotService {
   }
 
   // Fungsi untuk memicu Trial (misalnya mengubah status atau memindahkan data)
-  Future<void> activateTrial(String macAddress) async {
+  Future<void> activateTrial(String macAddress, String username, String ip) async {
+    final String waitPath = 'mikrotik_data/MatrixSphere/wait/$macAddress';
+    final String activePath = 'mikrotik_data/MatrixSphere/active/$macAddress';
+    Map<String, dynamic> updates = {};
+    updates[activePath] = {
+      'username': username,
+      'ip': ip,
+      'status': 'active',
+      'activated_at': DateTime.now().toString(),
+    };
+    updates[waitPath] = null;
+    await _dbRef.update(updates);
     try {
       // Logika: Pindahkan dari 'wait' ke 'active'
       // Atau sekadar kirim flag ke Mikrotik melalui Firebase
@@ -21,8 +32,10 @@ class HotspotService {
       });
 
       // Hapus dari daftar tunggu
-      await _dbRef.child('mikrotik_data/MatrixSphere/wait/$macAddress').remove();
-      
+      await _dbRef
+          .child('mikrotik_data/MatrixSphere/wait/$macAddress')
+          .remove();
+
       _logger.i("Trial berhasil diaktifkan untuk $macAddress");
     } catch (e) {
       _logger.e("Error mengaktifkan trial: $e");
