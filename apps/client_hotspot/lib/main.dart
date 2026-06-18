0cb3bbb2baff09fe4bc25f0f4680474e2ec6f054
dani6385 <dani6385@gmail.com>
2026-06-19 01:10:28 +0700

    data baru

--- apps/client_hotspot/lib/main.dart
+++ apps/client_hotspot/lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import '../screens/navigation_layout.dart';
+import 'firebase_options.dart';
 
 void main() async {
+  // Di apps/admin_mikrotik/lib/main.dart
+await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   // Jadikan main sebagai fungsi async
   // Pastikan Flutter binding telah diinisialisasi
   WidgetsFlutterBinding.ensureInitialized();
--- apps/client_hotspot/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/client_hotspot/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -15,7 +15,6 @@ import firebase_crashlytics
 import firebase_database
 import mobile_scanner
 import network_info_plus
-import path_provider_foundation
 import shared_preferences_foundation
 import url_launcher_macos
 
@@ -30,7 +29,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
