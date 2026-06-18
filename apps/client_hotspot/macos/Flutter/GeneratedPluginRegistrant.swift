67c4a39d1014c544dd4f9df1bade0dbe78526fd4
dani6385 <dani6385@gmail.com>
2026-06-18 18:19:55 +0000

    login mikrotik

--- apps/client_hotspot/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/client_hotspot/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -15,6 +15,7 @@ import firebase_crashlytics
 import firebase_database
 import mobile_scanner
 import network_info_plus
+import path_provider_foundation
 import shared_preferences_foundation
 import url_launcher_macos
 
@@ -29,6 +30,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
