9509fe5a10421cfa9514644d873a9aafdb7507bd
dani6385 <dani6385@gmail.com>
2026-06-21 08:47:41 +0000

    project telah di perbaiki

--- apps/client_hotspot/lib/main.dart
+++ apps/client_hotspot/lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'screens/permission_request_screen.dart';
-import 'screens/home_screen.dart';
-import 'providers/theme_provider.dart';
+import 'navigation_layout.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,56 +10,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => ThemeProvider(),
-      child: Consumer<ThemeProvider>(
-        builder: (context, themeProvider, child) {
-          return MaterialApp(
-            title: 'Hotspot Client',
-            theme: themeProvider.getTheme(),
-            debugShowCheckedModeBanner: false,
-            home: const InitialScreen(),
-          );
-        },
+    return MaterialApp(
+      title: 'Admin Mikrotik',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true, // Recommended for modern UI
       ),
-    );
-  }
-}
-
-class InitialScreen extends StatefulWidget {
-  const InitialScreen({super.key});
-
-  @override
-  State<InitialScreen> createState() => _InitialScreenState();
-}
-
-class _InitialScreenState extends State<InitialScreen> {
-  @override
-  void initState() {
-    super.initState();
-    _checkPermissionsAndNavigate();
-  }
-
-  Future<void> _checkPermissionsAndNavigate() async {
-    final status = await Permission.notification.status;
-    if (!mounted) return;
-    if (status.isGranted) {
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => const HomeScreen()),
-      );
-    } else {
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => const PermissionRequestScreen()),
-      );
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: CircularProgressIndicator(),
-      ),
+      home: const NavigationLayout(), // Set NavigationLayout as the home screen
     );
   }
 }
