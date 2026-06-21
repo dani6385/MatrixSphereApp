1b4da4247e659d804762a748f9784bf9911d819b
dani6385 <dani6385@gmail.com>
2026-06-21 06:43:02 +0000

    login mikrotik

--- apps/client_hotspot/lib/main.dart
+++ apps/client_hotspot/lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'screens/permission_request_screen.dart';
-import 'screens/home_screen.dart';
 import 'providers/theme_provider.dart';
+import 'screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,52 +15,31 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (_) => ThemeProvider(),
       child: Consumer<ThemeProvider>(
-        builder: (context, themeProvider, child) {
+        builder: (context, provider, child) {
           return MaterialApp(
-            title: 'Hotspot Client',
-            theme: themeProvider.getTheme(),
-            debugShowCheckedModeBanner: false,
-            home: const InitialScreen(),
+            title: 'M|S Connectivity',
+            // Use the isDarkMode boolean to set the themeMode
+            themeMode: provider.isDarkMode ? ThemeMode.dark : ThemeMode.light,
+            theme: ThemeData(
+              brightness: Brightness.light,
+              primarySwatch: Colors.blue,
+              visualDensity: VisualDensity.adaptivePlatformDensity,
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+              useMaterial3: true,
+            ),
+            darkTheme: ThemeData(
+              brightness: Brightness.dark,
+              primarySwatch: Colors.blue,
+              visualDensity: VisualDensity.adaptivePlatformDensity,
+              colorScheme: ColorScheme.fromSeed(
+                seedColor: Colors.blueAccent,
+                brightness: Brightness.dark,
+              ),
+              useMaterial3: true,
+            ),
+            home: const HomeScreen(),
           );
         },
-      ),
-    );
-  }
-}
-
-class InitialScreen extends StatefulWidget {
-  const InitialScreen({super.key});
-
-  @override
-  State<InitialScreen> createState() => _InitialScreenState();
-}
-
-class _InitialScreenState extends State<InitialScreen> {
-  @override
-  void initState() {
-    super.initState();
-    _checkPermissionsAndNavigate();
-  }
-
-  Future<void> _checkPermissionsAndNavigate() async {
-    final status = await Permission.notification.status;
-    if (!mounted) return;
-    if (status.isGranted) {
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => const HomeScreen()),
-      );
-    } else {
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => const PermissionRequestScreen()),
-      );
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: CircularProgressIndicator(),
       ),
     );
   }
