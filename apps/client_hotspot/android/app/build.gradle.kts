d425dd53220cde434f1593b446eec6c0f3bde247
dani6385 <dani6385@gmail.com>
2026-06-24 10:12:51 +0700

    fix build.gradle xml

--- apps/client_hotspot/android/app/build.gradle.kts
+++ apps/client_hotspot/android/app/build.gradle.kts
@@ -1,8 +1,13 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+// PERBAIKAN: Menambahkan JVM Toolchain untuk menyatukan versi Java untuk Kotlin dan Java.
+// Ini adalah cara modern untuk mengatasi error "Inconsistent JVM Target Compatibility".
+kotlin {
+    jvmToolchain(17)
 }
 
 android {
@@ -10,19 +15,10 @@ android {
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    // PERBAIKAN: Menghapus blok kotlinOptions yang sudah usang.
-    // Versi target JVM Kotlin sekarang secara otomatis diambil dari compileOptions di atas.
+    // Blok compileOptions tidak lagi diperlukan karena sudah diatur oleh toolchain di atas.
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.clientconnectivity"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -31,8 +27,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
