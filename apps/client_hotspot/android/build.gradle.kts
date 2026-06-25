6fb655ec19eb22ac2922c5ecef1f5eb37726c266
dani6385 <dani6385@gmail.com>
2026-06-25 12:19:18 +0700

    fix build.gradle xml

--- apps/client_hotspot/android/build.gradle.kts
+++ apps/client_hotspot/android/build.gradle.kts
@@ -29,6 +29,31 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+// Fix for third-party plugins that don't specify a namespace (required by AGP 8+).
+// Reads the package attribute from AndroidManifest.xml and sets it as the namespace.
+subprojects {
+    afterEvaluate {
+        if (project.hasProperty("android")) {
+            val android = project.extensions.findByName("android")
+            if (android is com.android.build.gradle.LibraryExtension) {
+                if (android.namespace.isNullOrEmpty()) {
+                    val manifestFile = file("${project.projectDir}/src/main/AndroidManifest.xml")
+                    if (manifestFile.exists()) {
+                        val packageName = javax.xml.parsers.DocumentBuilderFactory.newInstance()
+                            .newDocumentBuilder()
+                            .parse(manifestFile)
+                            .documentElement
+                            .getAttribute("package")
+                        if (packageName.isNotEmpty()) {
+                            android.namespace = packageName
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
