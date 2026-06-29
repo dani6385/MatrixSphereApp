5c12c198dd4276b977e655fb8b1bb985542c53d6
dani6385 <dani6385@gmail.com>
2026-06-29 21:51:40 +0700

    push and get

--- apps/client_connectivity/test/widget_test.dart
+++ apps/client_connectivity/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mikrologin/main.dart';
+import 'package:client_connectivity/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
