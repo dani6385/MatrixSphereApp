ebb93c08315000e331bbac09deec8bcaa14cae96
dani6385 <dani6385@gmail.com>
2026-07-02 16:13:41 +0700

    ya saya harapkan bisa

--- apps/client_connectivity/android/app/build.gradle.kts
+++ apps/client_connectivity/android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.client_connectivity"
+    namespace = "com.matrixsphere.connectivity"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.client_connectivity"
+        applicationId = "com.matrixsphere.connectivity"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
