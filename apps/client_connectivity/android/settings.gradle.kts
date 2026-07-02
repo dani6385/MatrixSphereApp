9b98d3d465ade78a333803f5eb50aceca8f7f9f6
dani6385 <dani6385@gmail.com>
2026-07-02 06:11:49 +0000

    Silakan coba build kembali proyek Anda. Saya akan menunggu hasilnya di sini dengan napas tertahan.

--- apps/client_connectivity/android/settings.gradle.kts
+++ apps/client_connectivity/android/settings.gradle.kts
@@ -1,26 +1,33 @@
 pluginManagement {
-    val flutterSdkPath =
-        run {
+    val flutterSdkPath = run {
+        val localPropertiesFile = File(settings.rootDir, "../local.properties")
+        if (localPropertiesFile.isFile) {
             val properties = java.util.Properties()
-            file("local.properties").inputStream().use { properties.load(it) }
-            val flutterSdkPath = properties.getProperty("flutter.sdk")
-            require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-            flutterSdkPath
+            localPropertiesFile.inputStream().use { properties.load(it) }
+            val sdkPath = properties.getProperty("flutter.sdk")
+            require(sdkPath != null) { "flutter.sdk not set in local.properties" }
+            sdkPath
+        } else {
+            System.getenv("FLUTTER_SDK") ?: throw GradleException("Flutter SDK not found. Define location with flutter.sdk in the local.properties file or with a FLUTTER_SDK environment variable.")
         }
+    }
 
-    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+    includeBuild(File(flutterSdkPath, "packages/flutter_tools/gradle"))
 
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
     }
+    plugins {
+        id("com.android.application") version "8.4.1" apply false
+        id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    }
 }
 
 plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.4.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.20" apply false
+    id("dev.flutter.flutter-plugin-loader")
 }
 
+rootProject.name = "android"
 include(":app")
