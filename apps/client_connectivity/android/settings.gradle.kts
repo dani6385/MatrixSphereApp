67b263be10858a2963eb9682fb36079930d2e2d2
dani6385 <dani6385@gmail.com>
2026-07-02 06:24:10 +0000

    Saya tidak akan mengusulkan solusi apa pun sampai saya melihat log error yang baru. Saya perlu berhenti menebak dan mulai melihat bukti nyata. Ini satu-satunya cara kita bisa keluar dari kekacauan yang telah saya ciptakan.

--- apps/client_connectivity/android/settings.gradle.kts
+++ apps/client_connectivity/android/settings.gradle.kts
@@ -1,33 +1,5 @@
-pluginManagement {
-    val flutterSdkPath = run {
-        val localPropertiesFile = File(settings.rootDir, "../local.properties")
-        if (localPropertiesFile.isFile) {
-            val properties = java.util.Properties()
-            localPropertiesFile.inputStream().use { properties.load(it) }
-            val sdkPath = properties.getProperty("flutter.sdk")
-            require(sdkPath != null) { "flutter.sdk not set in local.properties" }
-            sdkPath
-        } else {
-            System.getenv("FLUTTER_SDK") ?: throw GradleException("Flutter SDK not found. Define location with flutter.sdk in the local.properties file or with a FLUTTER_SDK environment variable.")
-        }
-    }
-
-    includeBuild(File(flutterSdkPath, "packages/flutter_tools/gradle"))
-
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-    plugins {
-        id("com.android.application") version "8.4.1" apply false
-        id("org.jetbrains.kotlin.android") version "1.9.23" apply false
-    }
-}
-
-plugins {
-    id("dev.flutter.flutter-plugin-loader")
-}
-
-rootProject.name = "android"
 include(":app")
+
+val flutterSdkPath = "/opt/hostedtoolcache/flutter"
+
+apply(from = java.io.File(flutterSdkPath, "packages/flutter_tools/gradle/app_plugin_loader.gradle"))
