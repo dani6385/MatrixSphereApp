eec8cd6e63f693d0a57a67c53debbec970bd0e0a
dani6385 <dani6385@gmail.com>
2026-07-02 06:47:00 +0000

    Ini akan menyelesaikan masalah "Could not read script" tersebut secara permanen.

--- apps/client_connectivity/android/settings.gradle.kts
+++ apps/client_connectivity/android/settings.gradle.kts
@@ -1,5 +1,24 @@
+pluginManagement {
+    val flutterSdkPath = run {
+        val properties = java.util.Properties()
+        file("local.properties").takeIf { it.exists() }?.inputStream()?.use { properties.load(it) }
+        properties.getProperty("flutter.sdk") ?: System.getenv("FLUTTER_ROOT")
+        ?: throw GradleException("Flutter SDK not found. Define location with flutter.sdk in local.properties or with a FLUTTER_ROOT environment variable.")
+    }
+
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
+    id("com.android.application") version "8.4.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+}
+
 include(":app")
-
-val flutterSdkPath = "/opt/hostedtoolcache/flutter"
-
-apply(from = java.io.File(flutterSdkPath, "packages/flutter_tools/gradle/app_plugin_loader.gradle"))
