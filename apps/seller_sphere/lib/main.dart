878585c7f1897ad9867cfc3a56a1e96da1969fed
dani6385 <dani6385@gmail.com>
2026-07-11 21:01:37 +0000

    push

--- apps/seller_sphere/lib/main.dart
+++ apps/seller_sphere/lib/main.dart
@@ -1,21 +1,30 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:seller_sphere/data/dao.dart';
 import 'package:seller_sphere/data/database.dart'
     hide ProductDao, TargetDao, TransactionDao;
 import 'package:seller_sphere/data/repository.dart';
-import 'package:seller_sphere/screens/dashboard_screen.dart';
+import 'package:seller_sphere/screens/main_scafold.dart';
 import 'package:seller_sphere/viewmodels/app_view_model.dart';
 import 'package:shared_ui/shared_ui.dart';
+import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
+import 'package:sqflite/sqflite.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    databaseFactory = databaseFactoryFfiWeb;
+  }
+
   final database = await DatabaseProvider.db.database;
   final repository = AppRepository(
     database.productDao as ProductDao,
     database.transactionDao as TransactionDao,
     database.targetDao as TargetDao,
   );
+
   runApp(
     ChangeNotifierProvider(
       create: (context) => AppViewModel(repository),
@@ -36,12 +45,7 @@ class SellerSphereApp extends StatelessWidget {
       themeMode: Provider.of<AppViewModel>(context).isDarkTheme
           ? ThemeMode.dark
           : ThemeMode.light,
-      home: DashboardScreen(
-        onNavigateToInventory: () {},
-        onNavigateToTransactions: () {},
-        onNavigateToChat: (String p1) {},
-        onNavigateToSlides: () {}, onNavigateToLive: () {  },
-      ),
+      home: const MainAppScaffold(),
     );
   }
 }
