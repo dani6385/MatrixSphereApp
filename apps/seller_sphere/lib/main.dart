2730f4b2ccdc6b65c659088fd92aee52668460ce
dani6385 <dani6385@gmail.com>
2026-07-13 09:04:02 +0000

    heh

--- apps/seller_sphere/lib/main.dart
+++ apps/seller_sphere/lib/main.dart
@@ -1,11 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:seller_sphere/app_scaffold.dart';
 import 'package:shared_ui/shared_ui.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Wajib ada!
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
