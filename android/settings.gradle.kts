9be8570bc9ba6620eea692c67f805f0d7c6f8476
dani6385 <dani6385@gmail.com>
2026-07-14 17:42:04 +0000

    mungkin

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -24,6 +24,7 @@ plugins {
     id("com.google.gms.google-services") version("4.4.4") apply false
     // END: FlutterFire Configuration
     id("org.jetbrains.kotlin.android") version "2.3.20" apply false
+    id("com.google.firebase.crashlytics") version "3.0.2" apply false
 }
 
 include(":app")
