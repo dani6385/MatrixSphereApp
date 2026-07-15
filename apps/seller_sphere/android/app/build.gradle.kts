b484ebe75aa021e86f9975304eb9f424543c9271
dani6385 <dani6385@gmail.com>
2026-07-15 05:13:41 +0000

    Changes before Firebase Studio auto-run

--- apps/seller_sphere/android/app/build.gradle.kts
+++ apps/seller_sphere/android/app/build.gradle.kts
@@ -56,28 +56,3 @@ dependencies {
     implementation("com.google.firebase:firebase-crashlytics")
     implementation("com.google.firebase:firebase-analytics")
 }
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
-}
