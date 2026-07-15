a5d709e96d4e0ed631629fbe4ba334a647f5d6bb
dani6385 <dani6385@gmail.com>
2026-07-15 07:28:24 +0000

    cob 1.9.20

--- apps/seller_sphere/android/settings.gradle.kts
+++ apps/seller_sphere/android/settings.gradle.kts
@@ -22,7 +22,7 @@ plugins {
     id("com.android.application") version "9.0.1" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
     id("com.google.firebase.crashlytics") version "3.0.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 }
 
 include(":app")
