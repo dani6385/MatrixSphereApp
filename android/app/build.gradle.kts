dc56413457770c5a771c6dae913d311a64d87c54
dani6385 <dani6385@gmail.com>
2026-07-16 11:26:57 +0000

    push

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.matrix_sphere"
+        applicationId = "com.matrix.sphere"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
