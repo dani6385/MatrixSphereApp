96a386ef27c266bf981e0854d9475fd783fc9b1f
dani6385 <dani6385@gmail.com>
2026-07-16 00:12:52 +0000

    sembilan

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,40 +2,32 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("dev.flutter.flutter-gradle-plugin")
-    // Panggil tanpa menuliskan 'version' lagi
-    id("com.google.gms.google-services")
-    id("com.google.firebase.crashlytics")
 }
 
 android {
-    namespace = "com.matrix.sphere"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    namespace = "com.example.matrix_sphere_app"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.example.matrix_sphere_app"
+        minSdk = 21
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+    }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
     kotlinOptions {
-        jvmTarget = "17"
-    }
-
-    defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.matrix.sphere"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        jvmTarget = "1.8"
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            isMinifyEnabled = false
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -45,11 +37,4 @@ flutter {
     source = "../.."
 }
 
-dependencies {
-    // Import Firebase BoM (Bill of Materials)
-    implementation(platform("com.google.firebase:firebase-bom:33.1.1"))
-
-    // Tambahkan library Crashlytics dan Analytics
-    implementation("com.google.firebase:firebase-crashlytics")
-    implementation("com.google.firebase:firebase-analytics")
-}
+dependencies {}
--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,23 +1,18 @@
-pluginManagement {
-    val flutterSdkPath =
-        run {
-            val properties = java.util.Properties()
-            val localPropertiesFile = file("local.properties")
-            
-            // Cek jika file local.properties ada (biasanya di laptop lokal)
-            if (localPropertiesFile.exists()) {
-                localPropertiesFile.inputStream().use { properties.load(it) }
-            }
-            
-            // Ambil dari local.properties, jika tidak ada (seperti di GitHub), ambil dari Environment Variable
-            val sdkPath = properties.getProperty("flutter.sdk") ?: System.getenv("FLUTTER_ROOT")
-            
-            require(sdkPath != null) { "Flutter SDK not found. Define 'flutter.sdk' in local.properties or set 'FLUTTER_ROOT' environment variable." }
-            sdkPath
+val flutterSdkPath =
+    run {
+        val properties = java.util.Properties()
+        val localPropertiesFile = file("local.properties")
+        if (localPropertiesFile.exists()) {
+            localPropertiesFile.inputStream().use { properties.load(it) }
         }
+        val sdkPath = properties.getProperty("flutter.sdk") ?: System.getenv("FLUTTER_ROOT")
+        require(sdkPath != null) { "Flutter SDK not found. Define 'flutter.sdk' in local.properties or set 'FLUTTER_ROOT' environment variable." }
+        sdkPath
+    }
 
-    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
+pluginManagement {
     repositories {
         google()
         mavenCentral()
@@ -29,11 +24,8 @@ plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    
-    // START: FlutterFire Configuration
     id("com.google.gms.google-services") version "4.4.1" apply false
     id("com.google.firebase.crashlytics") version "2.9.9" apply false
-    // END: FlutterFire Configuration
 }
 
 include(":app")
