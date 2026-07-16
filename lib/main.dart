72623cd5c7d4e9253ecf1d89344cc1c170301060
dani6385 <dani6385@gmail.com>
2026-07-16 12:23:22 +0000

    siap

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import '../navigation/app_navigator.dart';
-import 'firebase_options.dart';
+import 'package:shared_services/shared_services.dart';
 
 void main() async {
   runZonedGuarded<Future<void>>(() async {
---
+++ packages/shared_services/lib/shared_services.dart
@@ -0,0 +1,1 @@
+export 'services/firebase_options.dart';
