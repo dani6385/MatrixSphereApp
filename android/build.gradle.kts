3a44bf169bbca7ec256d5546c839fec798c94a88
dani6385 <dani6385@gmail.com>
2026-07-17 11:11:11 +0000

    maju

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -6,6 +6,8 @@ buildscript {
     dependencies {
         // Menggunakan tanda petik ganda dan tanda kurung untuk Kotlin DSL
         classpath("com.android.tools.build:gradle:8.4.0")
+        // Menambahkan classpath untuk plugin Kotlin
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
     }
 }
 
