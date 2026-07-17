ad975fdc23a43c775079c12a55b2371959172919
dani6385 <dani6385@gmail.com>
2026-07-17 15:28:14 +0000

    PUSH

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -24,7 +24,7 @@ allprojects {
 
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
-        .dir("../../build")
+        .dir("../build")
         .get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
