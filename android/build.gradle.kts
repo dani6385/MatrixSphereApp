7f41926c3ab133c8293cc2b6c7c12e44131c3b37
dani6385 <dani6385@gmail.com>
2026-07-18 10:50:27 +0000

    hehe

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,19 +1,16 @@
-// File: android/build.gradle
-
 buildscript {
-    ext.kotlin_version = "1.8.22"
     repositories {
         google()
         mavenCentral()
     }
-
     dependencies {
-        classpath("com.android.tools.build:gradle:7.4.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+        classpath("com.android.tools.build:gradle:8.4.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
     }
 }
-// ... sisa file
 
+# TAMBAHKAN BARIS DI BAWAH INI UNTUK MENYUPLAI VERSI KOTLIN GLOBAL
+rootProject.extra.set("kotlin_version", "1.9.24")
 
 allprojects {
     repositories {
@@ -24,26 +21,17 @@ allprojects {
 
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
-        .dir("../build")
+        .dir("../../build")
         .get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
 
-    // Konfigurasi Android harus dilakukan SEBELUM evaluationDependsOn
-    plugins.withType<com.android.build.gradle.BasePlugin> {
-        extensions.configure<com.android.build.gradle.BaseExtension> {
-            compileOptions.sourceCompatibility = JavaVersion.VERSION_1_8
-            compileOptions.targetCompatibility = JavaVersion.VERSION_1_8
-        }
-    }
-
-    // Hanya panggil evaluationDependsOn jika ini bukan proyek :app itu sendiri
-    if (project.name != "app") {
-        evaluationDependsOn(":app")
-    }
+subprojects {
+    project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
