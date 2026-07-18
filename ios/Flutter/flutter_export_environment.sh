72b608e4f412dfaf8fb5b64a01ef69f813cc3501
dani6385 <dani6385@gmail.com>
2026-07-19 04:22:35 +0700

    lah lahan lah

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/user/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/user/myapp"
-export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=/home/user/myapp/ios/Flutter/ephemeral/Packages/.packages/FlutterFramework"
+export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\MatrixSphereApp"
+export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=D:\MatrixSphereApp\ios\Flutter\ephemeral\Packages\.packages\FlutterFramework"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
