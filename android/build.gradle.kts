14af50e1857dcc9981830ebba66c95f7ec8b598d
dani6385 <dani6385@gmail.com>
2026-07-19 15:12:25 +0000

    maju

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,16 +1,12 @@
 // android/build.gradle.kts
 
 plugins {
-    // Plugin default bawaan Flutter
-    id("com.android.application") version "8.1.0" apply false
-    id("com.android.library") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
-    
-    // Tambahkan plugin Google Services
-    id("com.google.gms.google-services") version "4.4.1" apply false
-    
-    // Tambahkan plugin Firebase Crashlytics
-    id("com.google.firebase.crashlytics") version "2.9.9" apply false
+    // Hapus nomor versi (seperti version "8.1.0") dari sini
+    id("com.android.application") apply false
+    id("com.android.library") apply false
+    id("org.jetbrains.kotlin.android") apply false
+    id("com.google.gms.google-services") apply false
+    id("com.google.firebase.crashlytics") apply false
 }
 
 allprojects {
--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,3 +1,5 @@
+// android/settings.gradle.kts
+
 pluginManagement {
     val flutterSdkPath =
         run {
@@ -20,11 +22,14 @@ pluginManagement {
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "9.0.1" apply false
+    id("com.android.library") version "9.0.1" apply false // Ditambahkan untuk library android
+    
     // START: FlutterFire Configuration
-    id("com.google.gms.google-services") version("4.4.4") apply false
-    id("com.google.firebase.crashlytics") version("3.0.7") apply false
+    id("com.google.gms.google-services") version "4.4.4" apply false
+    id("com.google.firebase.crashlytics") version "3.0.7" apply false
     // END: FlutterFire Configuration
-    id("org.jetbrains.kotlin.android") version "2.3.20" apply false
+    
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false // Disarankan menggunakan versi stabil seperti 1.9.24 atau 2.0.20
 }
 
 include(":app")
