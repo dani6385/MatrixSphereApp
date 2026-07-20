1bfcacbb68012523c66a304d96bce17ec7fd486a
dani6385 <dani6385@gmail.com>
2026-07-20 11:21:10 +0000

    tunggu

--- lib/screens/setting/widgets/setting_body.dart
+++ lib/screens/setting/widgets/setting_body.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_ui/shared_ui.dart';
 
-
 class SettingBody extends StatelessWidget {
   const SettingBody({super.key});
 
