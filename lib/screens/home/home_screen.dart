42e605c7a77d8efa69bb051aed4ca4f65dbb8ea8
dani6385 <dani6385@gmail.com>
2026-07-20 07:27:28 +0700

    Update home_screen.dart

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,41 +1,24 @@
+// lib/screens/home/home_screen.dart
+
 import 'package:flutter/material.dart';
 import 'package:shared_ui/shared_ui.dart';
-import 'package:provider/provider.dart';
-import '../chat/providers/chat_provider.dart';
+
 import '../account/widgets/account_menu_modal.dart';
 import 'widgets/home_app_bar.dart';
 import 'widgets/home_body.dart';
-import 'widgets/home_content.dart';
-import 'widgets/featured_card.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-        create: (_) =>
-            ChatProvider(), // Menginisialisasi ChatProvider hanya untuk halaman ini
-        child: const Scaffold(
-          backgroundColor: kLightSurface,
-          appBar: const HomeAppBar(),
-          drawer: const AccountMenuModal(),
-          body: const Column(
-            children: [
-              // 1. Widget Atas: FeaturedCard (mengikuti tinggi asli widgetnya)
-              FeaturedCard(),
-              SizedBox(height: 12), // Jarak pemisah tengah
-              Expanded(
-                child: Column(
-                  children: [
-                    // 2. Widget Bawah: HomeBody (mengisi sisa ruang yang tersedia)
-                    HomeBody(),
-                    HomeContent(),
-                  ],
-                ),
-              )
-            ],
-          ),
-        ));
+    return const Scaffold(
+      backgroundColor: kLightSurface,
+      appBar: HomeAppBar(),
+      drawer: AccountMenuModal(),
+      
+      // Menggunakan HomeBody murni untuk menghindari crash tata letak
+      body: HomeBody(), 
+    );
   }
 }
