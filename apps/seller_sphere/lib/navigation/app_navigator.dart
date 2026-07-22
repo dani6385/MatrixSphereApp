a2b1db4f9e815bd1c9702635193dc73760f068b1
dani6385 <dani6385@gmail.com>
2026-07-23 01:58:37 +0700

    jadi

--- apps/seller_sphere/lib/navigation/app_navigator.dart
+++ apps/seller_sphere/lib/navigation/app_navigator.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-//import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:google_nav_bar/google_nav_bar.dart';
+//import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:provider/provider.dart'; // <-- 1. IMPORT PROVIDER
-import 'package:shared_ui/shared_ui.dart';
 import 'package:seller_sphere/screens/chat/Providers/chat_provider.dart';
+import 'package:shared_ui/shared_ui.dart';
 import '../screens/attendance/providers/attendance_provider.dart';
 
 class AppNavigator extends StatefulWidget {
@@ -17,10 +17,6 @@ class AppNavigator extends StatefulWidget {
 }
 
 class _AppNavigatorState extends State<AppNavigator> {
-  void _onItemTapped(int index) {
-    widget.navigationShell.goBranch(index);
-  }
-
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
@@ -31,38 +27,23 @@ class _AppNavigatorState extends State<AppNavigator> {
       child: Scaffold(
         extendBody: true,
         body: widget.navigationShell,
-        // --- Kembali menggunakan CustomBottomNavigationBar yang sudah bisa di-styling ---
+        // --- Menggunakan CustomBottomNavigationBar dengan Icon dan warna ---
         bottomNavigationBar: CustomBottomNavigationBar(
-          selectedIndex: widget.navigationShell.currentIndex,
-          onItemTapped: _onItemTapped,
-          // Sekarang Anda bisa mengatur warna di sini!
-          activeColor: Colors.deepPurple, // Contoh: Ubah warna aktif
-          color: Colors.grey[800], // Contoh: Ubah warna tidak aktif
-          tabBackgroundColor: Colors.deepPurple.withOpacity(0.15), // Contoh: Ubah warna latar tab
+          initialSelectedIndex: widget.navigationShell.currentIndex,
+          screens: widget.navigationShell.indexedStack.children,
           tabs: const [
-            GButton(
-              icon: Icons.home,
-              text: 'Beranda',
-            ),
-            GButton(
-              icon: Icons.live_tv,
-              text: 'Live',
-            ),
-            GButton(
-              icon: Icons.point_of_sale,
-              text: 'Kasir',
-            ),
-            GButton(
-              icon: Icons.inventory_2,
-              text: 'Inventori',
-            ),
-            GButton(
-              icon: Icons.how_to_reg,
-              text: 'Absensi',
-            ),
+            GButton(icon: Icons.home, text: 'Home'),
+            GButton(icon: Icons.live_tv, text: 'Stream'),
+            GButton(icon: Icons.point_of_sale, text: 'Sellers'),
+            GButton(icon: Icons.inventory_2, text: 'Inventory'),
+            GButton(icon: Icons.how_to_reg, text: 'Attendance'),
           ],
         ),
       ),
     );
   }
+}
+
+extension on StatefulNavigationShell {
+  get indexedStack => null;
 }
--- packages/shared_ui/lib/widgets/custom_bottom_navigation_bar.dart
+++ packages/shared_ui/lib/widgets/custom_bottom_navigation_bar.dart
@@ -1,36 +1,75 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:shared_ui/shared_ui.dart';
 
-class CustomBottomNavigationBar extends StatelessWidget {
-  final int selectedIndex;
-  final Function(int) onItemTapped;
-  final List<Widget> items; // Diubah dari List<GButton> menjadi List<Widget>
-  final Color? activeColor;
-  final Color? backgroundColor;
+class CustomBottomNavigationBar extends StatefulWidget {
+  final List<Widget> screens;
+  final List<GButton> tabs;
+  final int initialSelectedIndex;
 
   const CustomBottomNavigationBar({
     super.key,
-    required this.selectedIndex,
-    required this.onItemTapped,
-    required this.items,
-    this.activeColor,
-    this.backgroundColor,
+    required this.screens,
+    required this.tabs,
+    this.initialSelectedIndex = 0,
   });
 
   @override
+  State<CustomBottomNavigationBar> createState() =>
+      _CustomBottomNavigationBarState();
+}
+
+class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
+  late int _selectedIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedIndex = widget.initialSelectedIndex;
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // Menggunakan CurvedNavigationBar untuk efek gelombang
-    return CurvedNavigationBar(
-      index: selectedIndex,
-      height: 60.0,
-      items: items,
-      color: backgroundColor ?? Colors.white,
-      buttonBackgroundColor: backgroundColor ?? Colors.white,
-      backgroundColor: Colors.transparent, // Latar belakang utama transparan
-      animationCurve: Curves.easeInOut,
-      animationDuration: const Duration(milliseconds: 400),
-      onTap: onItemTapped,
-      letIndexChange: (index) => true,
+    final items = widget.tabs
+        .map((tab) => Icon(tab.icon, size: 24, color: kDarkTextPrimary))
+        .toList();
+
+    return Scaffold(
+      extendBody: true,
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: widget.screens,
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: kBrandPrimary.withOpacity(0.3),
+              spreadRadius: 5,
+              blurRadius: 10,
+              offset: const Offset(0, -3), // changes position of shadow
+            ),
+          ],
+        ),
+        child: CurvedNavigationBar(
+          index: _selectedIndex,
+          height: 60,
+          items: items,
+          onTap: _onItemTapped,
+          color: kNeonCyan,
+          buttonBackgroundColor: kTransparent, // Menggunakan kBrandPrimary
+          backgroundColor: kLightBackground,
+          animationCurve: Curves.easeInOut,
+          animationDuration: const Duration(milliseconds: 400),
+        ),
+      ),
     );
   }
 }
