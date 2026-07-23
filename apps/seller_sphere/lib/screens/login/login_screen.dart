d09bedc9ac44668b312da5737af15b287c23a001
dani6385 <dani6385@gmail.com>
2026-07-24 06:36:01 +0700

    cik

--- apps/seller_sphere/lib/screens/login/login_screen.dart
+++ apps/seller_sphere/lib/screens/login/login_screen.dart
@@ -3,25 +3,24 @@
 import 'package:flutter/material.dart';
 import 'package:shared_ui/shared_ui.dart';
 
-
 import 'widgets/Login_app_bar.dart';
 import 'widgets/Login_body.dart';
-
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   // 1. DEKLARASIKAN GLOBALKEY DI SINI
-  static final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+  static final GlobalKey<ScaffoldState> scaffoldKey =
+      GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: scaffoldKey, // 2. PASANGKAN KEY KE SINI
-      backgroundColor: kDarkBackground,
+      backgroundColor: kBrandTertiary,
       appBar: const LoginAppBar(),
       //drawer: const SideMenu(selectedRoute: MenuRoute.account),
-      //endDrawer: const SettingScreen(), 
+      //endDrawer: const SettingScreen(),
       body: const LoginBody(),
       bottomNavigationBar: Container(
         padding: const EdgeInsets.all(16.0),
@@ -40,11 +39,13 @@ class LoginScreen extends StatelessWidget {
           child: Text(
             '© 2024 MatrixSphere. All rights reserved.',
             textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.bodySmall?.copyWith(color: Colors.grey[600]),
+            style: Theme.of(context)
+                .textTheme
+                .bodySmall
+                ?.copyWith(color: Colors.grey[600]),
           ),
         ),
       ),
-      
     );
   }
 }
