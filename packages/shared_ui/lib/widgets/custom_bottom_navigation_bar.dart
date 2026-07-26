62ce02f71da5dd629ec458093cf732cce0152b46
dani6385 <dani6385@gmail.com>
2026-07-26 18:26:45 +0700

    lihat

--- packages/shared_ui/lib/widgets/custom_bottom_navigation_bar.dart
+++ packages/shared_ui/lib/widgets/custom_bottom_navigation_bar.dart
@@ -39,8 +39,7 @@ class SharedBottomNavigationBar extends StatelessWidget {
         items: items,
         onTap: onItemTapped,
         color: kNeonCyan,
-        buttonBackgroundColor: kTransparent, // Mengubah warna latar tombol menjadi transparan
-        backgroundColor: kTransparent,
+        
         animationCurve: Curves.easeInOut,
         animationDuration: const Duration(milliseconds: 400),
       ),
