8a9ce3fea1ef4162174a53ed1ef6ed1274d2d059
dani6385 <dani6385@gmail.com>
2026-07-26 12:51:55 +0700

    masih kah

--- apps/seller_sphere/lib/navigation/app_routes.dart
+++ apps/seller_sphere/lib/navigation/app_routes.dart
@@ -12,24 +12,24 @@ class AppRoutes {
   static const String attendance = '/attendance';
   static const String sellers = '/sellers';
 
-  static const String attendanceProvider = '/attendance/providers';
-  static const String calendar = '/calendar';
-  static const String aktifitas = '/activity';
-  static const String status = '/Status'; // Pastikan rute Status ada
+
+  static const String access = '/access';
   static const String account = '/account'; // Rute baru untuk Akun
+  static const String aktifitas = '/activity';
   static const String approval = '/approval';
   static const String approvalDetail = '/approval/approval-detail';
-  static const String settings = '/settings';
+  static const String attendanceProvider = '/attendance/providers';
+  static const String calendar = '/calendar';
+  static const String camera = '/camera';
   static const String chat = '/chat';
   static const String chatprovider = 'providers/chat';
-  static const String camera = '/camera';
-  static const String access = '/access';
-  static const String goodsIn = '/goods-in'; // Rute untuk input barang
-  static const String manageStock = '/manage-stock'; // Rute untuk kelola stok
-  static const String goodsOut = '/goods-out'; // Rute untuk pengeluaran barang
-
-  static const String profile = '/profile';
   static const String editprofile = '/profile/editprofile';
+  static const String goodsIn = '/goods-in'; // Rute untuk input barang
+  static const String goodsOut = '/goods-out'; // Rute untuk pengeluaran barang
   static const String inventoryAdd = '/inventory/addproduct';
   static const String inventoryEdit = '/inventory/editproduct';
+  static const String manageStock = '/manage-stock'; // Rute untuk kelola stok
+  static const String profile = '/profile';
+  static const String settings = '/settings';
+  static const String status = '/Status'; // Pastikan rute Status ada
 }
