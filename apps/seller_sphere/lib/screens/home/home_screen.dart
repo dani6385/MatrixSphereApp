ccdd492a08619c78d3765feb4b1cf54beb51e3ba
dani6385 <dani6385@gmail.com>
2026-07-26 19:03:12 +0700

    ciknya

--- apps/seller_sphere/lib/screens/home/home_screen.dart
+++ apps/seller_sphere/lib/screens/home/home_screen.dart
@@ -46,4 +46,3 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
-
--- packages/shared_ui/lib/widgets/custom_bottom_navigation_bar.dart
+++ packages/shared_ui/lib/widgets/custom_bottom_navigation_bar.dart
@@ -39,7 +39,8 @@ class SharedBottomNavigationBar extends StatelessWidget {
         items: items,
         onTap: onItemTapped,
         color: kNeonCyan,
-        
+        //buttonBackgroundColor: kTransparent, // Mengubah warna latar tombol menjadi transparan
+        backgroundColor: kTransparent,
         animationCurve: Curves.easeInOut,
         animationDuration: const Duration(milliseconds: 400),
       ),
