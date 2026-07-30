c93c1cd34b0dc637ff03496c293de92994ee79ae
dani6385 <dani6385@gmail.com>
2026-07-30 15:08:00 +0700

    ketika

---
+++ apps/seller_sphere/lib/screens/home/components/home_appbar.dart
@@ -0,0 +1,28 @@
+// lib/screens/widgets/Home_app_bar.dart
+
+import 'package:flutter/material.dart';
+//import 'package:go_router/go_router.dart';
+//import 'package:shared_ui/shared_ui.dart';
+
+class HomeAppBar extends StatefulWidget implements PreferredSizeWidget {
+  const HomeAppBar({super.key});
+
+  @override
+  State<HomeAppBar> createState() => _HomeAppBarState();
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class _HomeAppBarState extends State<HomeAppBar> {
+  // Nilai awal yang terpilih di dropdown
+
+  // Daftar opsi menu yang diminta
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: const Text('Home'),
+    );
+  }
+}
--- apps/seller_sphere/lib/screens/home/home_screen.dart
+++ apps/seller_sphere/lib/screens/home/home_screen.dart
@@ -1,29 +1,105 @@
-// lib/screens/home/home_screen.dart
-
 import 'package:flutter/material.dart';
-//import 'package:shared_ui/shared_ui.dart';
-
-
-import 'widgets/home_app_bar.dart';
-import 'widgets/home_body.dart';
-//import 'package:seller_sphere/features/settings/setting_screen.dart';
+import 'package:go_router/go_router.dart';
+import 'package:shared_ui/shared_ui.dart';
+import 'components/home_appbar.dart';
+import 'components/home_body.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
-  // 1. DEKLARASIKAN GLOBALKEY DI SINI
-  static final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
-
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
     return Scaffold(
-      key: scaffoldKey,
-      backgroundColor: theme.scaffoldBackgroundColor,
       appBar: const HomeAppBar(),
-      //drawer: const SideMenu(selectedRoute: MenuRoute.account),
-      //endDrawer: const SettingScreen(), 
-      body: const HomeBody(),
+      drawer: SideMenu(
+        // 1. Definisikan Header kustom untuk Seller Sphere
+        header: const DrawerHeader(
+          decoration: BoxDecoration(color: kDarkAppBar),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Icon(Icons.storefront, color: kBrandPrimary, size: 48),
+              SizedBox(height: AppSpacing.sm),
+              Text(
+                'Seller Sphere',
+                style: TextStyle(
+                  color: kDarkTextPrimary,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+        // 2. Definisikan item menu dan logikanya
+        items: [
+          SideMenuItem(
+            title: 'Product',
+            icon: Icons.store,
+            isSelected: true, // Ganti dengan logika state Home Anda
+            onTap: () {
+              Navigator.of(context).pop(); // Tutup drawer
+              context.go('/product'); // Lakukan navigasi
+            },
+          ),
+          SideMenuItem(
+            title: 'Approval',
+            icon: Icons.playlist_add_check,
+            isSelected: false,
+            onTap: () {
+              Navigator.of(context).pop();
+              context.go('/approval');
+            },
+          ),
+          // ... item menu lainnya
+        ], selectedRoute: null,
+      ),
+      endDrawer: SideMenu(
+        // 1. Definisikan Header kustom untuk Seller Sphere
+        header: const DrawerHeader(
+          decoration: BoxDecoration(color: kDarkAppBar),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Icon(Icons.storefront, color: kBrandPrimary, size: 48),
+              SizedBox(height: AppSpacing.sm),
+              Text(
+                'Kalkulasi',
+                style: TextStyle(
+                  color: kDarkTextPrimary,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+        // 2. Definisikan item menu dan logikanya
+        items: [
+          SideMenuItem(
+            title: 'Edit',
+            icon: Icons.store,
+            isSelected: true, // Ganti dengan logika state Home Anda
+            onTap: () {
+              Navigator.of(context).pop(); // Tutup drawer
+              context.go('/product'); // Lakukan navigasi
+            },
+          ),
+          SideMenuItem(
+            title: 'Approval',
+            icon: Icons.playlist_add_check,
+            isSelected: false,
+            onTap: () {
+              Navigator.of(context).pop();
+              context.go('/approval');
+            },
+          ),
+          // ... item menu lainnya
+        ], selectedRoute: null,
+      ),
+      body: const HomeBody(), // ... body
     );
   }
 }
